8873c5249785692182913669c18b91205bade399
Nika Lukava <Nikolla-L2002@users.noreply.github.com>
2022-01-30 00:11:29 +0400

    static data for get request

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import ProductController from "../controllers/ProductController";
 
+import data from '../static_data/data.json'
+
 const router = express.Router({ mergeParams : true });
 
 router.post("/", ProductController.postProduct);
-router.get("/", ProductController.getProduct);
-// router.get("/:id", ProductController.getOneProduct);
+// router.get("/", ProductController.getProduct);
+router.get("/:id", ProductController.getOneProduct);
 router.put("/:id", ProductController.updateProduct);
 router.delete("/:id", ProductController.deleteProduct);
-router.get("/all", (req: any, res:any) => {
-    res.send('აქაა ყველა პროდუქტი')
+router.get("/", (req: any, res:any) => {
+    res.send(data)
 });
 
 export default router;
