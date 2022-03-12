d2170b64c07ea640f678d8c177c17a0f023498d1
Nika Lukava <76733267+Nikolla-L@users.noreply.github.com>
2022-03-12 15:49:15 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ createConnection()
     .then(async connection => {
         const app = express();
         app.use(cors({
-            origin: ["http://localhost:3000"]",
+            origin: ["http://localhost:3000"],
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
             credentials: true,
         }))
